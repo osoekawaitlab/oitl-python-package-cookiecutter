d72a66acba5779fe6453db4e4a883cd60842e927
osoken <osoken@osoekawalab.com>
2025-11-23 23:13:35 +0900

    fix: fix import order

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,7 +1,8 @@
 """{{ cookiecutter.project_description }}"""
+
+from {{ cookiecutter.package_name }}._version import __version__
 {% if cookiecutter.use_cli == "yes" %}
 from {{ cookiecutter.package_name }}.cli import main
 {% endif %}
-from {{ cookiecutter.package_name }}._version import __version__
 
 __all__ = ["__version__"{% if cookiecutter.use_cli == "yes" %}, "main"{% endif %}]
